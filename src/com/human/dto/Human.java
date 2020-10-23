41edbe235bb96dffc1412f6a4b283ea3a204fa26
Administrator <Administrator@WIN-JCT78BHHOI0.local>
2020-10-23 09:45:33 +0900

    201023

--- src/com/human/dto/Human.java
+++ src/com/human/dto/Human.java
@@ -1,6 +1,6 @@
 package com.human.dto;
 
-public class Class001 {
+public class Human {
 	public String name="";
 	public int age=0;
 	public double height=0;
--- src/com/human/ex/Class001.java
+++ src/com/human/ex/Class001.java
@@ -3,7 +3,7 @@ package com.human.ex;
 public class Class001 {
 
 	public static void main(String[] args) {
-		com.human.dto.Class001 c1=new com.human.dto.Class001(); //com.human.dto의 클래스 Class001
+		com.human.dto.Human c1=new com.human.dto.Human(); //com.human.dto의 클래스 Human
 		c1.name="김소현";
 		c1.age=26;
 		c1.height=158;
