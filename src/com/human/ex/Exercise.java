bb6f21c32cc7fc3a6a7432fb7e86721b8df0028c
Administrator <Administrator@WIN-JCT78BHHOI0.local>
2020-11-03 14:25:00 +0900

    201103

--- src/com/human/ex/Exercise.java
+++ src/com/human/ex/Exercise.java
@@ -3,102 +3,165 @@ package com.human.ex;
 import java.util.Scanner;
 
 public class Exercise {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		
-		String id[]=new String[100];
-		String pw[]=new String[100];
-		String name[]=new String[100];
-		double account[]=new double[100];
-		
-		String inputId=null;
-		String inputPw=null;
-		int indexCount=0;
-		
-		double inMoney=0;
-		double outMoney=0;
-		
-		
-		while(true) {
-			System.out.println("로그인 하세요");
-			System.out.print("ID >> ");
-			inputId=sc.nextLine();
-			System.out.print("PW >> ");
-			inputPw=sc.nextLine();
+	public static java.util.Scanner sc=new java.util.Scanner(System.in);
+	public static int deck[]=new int [52];
+	public static int p1Deck[]=new int[10];
+	public static int p2Deck[]=new int[10];
+	public static int deckIndex=52;
+	public static int p1DeckIndex=0;
+	public static int p2DeckIndex=0;
+	public static boolean isP1Game=true;
+	public static boolean isP2Game=true;
+	
+	public static String cardShape[]= {"스페이드", "다이아", "하트", "클로버"};
+	public static String cardNumber[]= {"A","","3","4","5","6","7","8","9","10","J","Q","K"};
+	
+	
+	public static void newCard() {
+		for(int i=0; i<deck.length; i++) {
+			deck[i]=i;
+		}
+	}
+	
+	public static void mixCard() {
+		for(int i=0; i<10000; i++) {
+			int randomIndex=(int)(Math.random());
+			int temp=deck[0];
+			deck[0]=deck[randomIndex];
+			deck[randomIndex]=temp;
+		}
+	}
+	
+	public static void displayP1Card() {
+		System.out.print("p1Card : ");
+		for(int i=0; i<p1DeckIndex; i++) {
+			System.out.print(cardShape[p1Deck[i]/13]+" "+ cardNumber[p1Deck[i]%13]+", ");
 			
-			if(inputId==null) {
-				
-			}else if(inputId.equals("exit")) {
-				break;
-			}else if(inputId.equals("admin")&&inputPw.equals("admin")) {
-				//관리자 계정
-				boolean isAdminLogin=true;
-				while(isAdminLogin) {
-					System.out.println("관리자 계정 입니다.");
-					System.out.println("메뉴를 입력 해주세요.");
-					System.out.println("1.계정 추가 2.계정 삭제 3.모든 사용자 정보 확인 4.id로 특정 사용자 정보 확인 5.종료");
-					int selectNum=Integer.parseInt(sc.nextLine());
+		}
+		System.out.println("현재 점수 : "+p1Scoure());
+	}
+	
+	public static void displayP2Card() {
+		System.out.print("p2 Card : ");
+		for(int i=0; i<p2DeckIndex; i++) {
+			System.out.println(cardShape[p2Deck[i]/13]+" "+cardNumber[p2Deck[i]%13]+", ");
+		}
+		System.out.println("현재 점수 : p2Scoure()");
+	}
+	
+	public static void getP1Card() {
+		p1Deck[p1DeckIndex]=deck[deckIndex-1];
+		deckIndex--; p1DeckIndex++;
+	}
+	
+	public static void getP2Card() {
+		p2Deck[p2DeckIndex]=deck[deckIndex-1];
+		deckIndex--; p2DeckIndex++;
+	}
+	
+	public static int p1Scoure() {
+		int returnValue=0;
+		for(int i=0; i<p1DeckIndex; i++) {
+			int countNum=p1Deck[i]%13+1;
+			if(countNum>10) {
+				countNum=10;
+			}
+			returnValue+=countNum;
+		}
+		for(int j=0; j<p1DeckIndex; j++) {
+			if(p1Deck[j]%13==0) {
+				if((returnValue+10)>21){
+				}else {
+					returnValue+=10;
 				}
-				
-			}else {
-				//일반 사용자 로그인
-				boolean isLogin=false;
-				for(int i=0; i<id.length; i++) {
-					if(id[i]!=null && id[i].equals(inputId)&&pw[i].equals(inputPw)) {
-						isLogin=true;
-						indexCount=i;
+				break;
+			}
+		}
+		return returnValue;
+	}
+	
+	public static int p2Scoure() {
+		int returnValue=0;
+		for(int i=0; i<p2DeckIndex; i++) {
+			int countNum=p2Deck[i]%13+1;
+			if(countNum>10) {
+				countNum=10;
+			}
+			returnValue+=countNum;
+		}
+		for(int j=0; j<p2DeckIndex; j++) {
+			if(p2Deck[j]%13==0) {
+				if(returnValue+10>21) {
+					
+				}else {
+					returnValue+=10;
+				}
+				break;
+			}
+			break;
+			
+		}
+		return returnValue;
+	}
+	
+	public static void play() {
+		for(int i=0; i<10; i++) {
+			System.out.println(i+1+"번째------------");
+			if(isP1Game) {
+				System.out.println("p1 player님 카드를 받으시겠습니까?(1.yes 2.no)");
+				if(1==Integer.parseInt(sc.nextLine())) {
+					getP1Card();
+					if(p1Scoure()>21) {
 						break;
 					}
+				}else {
+					isP1Game=false;
 				}
-				
-				if(isLogin) {
-					//로그인 성공
-					System.out.println(name[indexCount]+"님이 로그인 하셨습니다.");
-					System.out.println("");
-					while(isLogin) {
-						System.out.println("메뉴를 입력해 주세요");
-						System.out.println("1.입금 2.출금 3.조회 4.종료>>");
-						System.out.println("");
-						int selectNum=Integer.parseInt(sc.nextLine());
-						switch(selectNum) {
-						case 1:
-							System.out.print("입금하실 금액을 입력 해 주세요.>>");
-							inMoney=Double.parseDouble(sc.nextLine());
-							account[indexCount]+=inMoney;
-							System.out.println(name+"님께서 입금 하신 금액은 "+inMoney+"입니다.");
-							break;
-						case 2:
-							System.out.print("출금하실 금액을 입력 해 주세요.>>");
-							outMoney=Double.parseDouble(sc.nextLine());
-							if(account[indexCount]>outMoney) {
-								account[indexCount]-=outMoney;
-								System.out.println(name+"님께서 출금 하신 금액은 "+outMoney+"입니다.");
-							}else {
-								System.out.println("잔액이 부족합니다.");
-							}
-							break;
-						case 3:
-							System.out.println(name+"님의 현재 잔액은 "+account[indexCount]+"입니다.");
-							break;
-						case 4:
-							 System.out.println("로그아웃 되었습니다.");
-							 inputId=null;
-							 isLogin=false;
-							 break;
-						default:
-							System.out.println("잘못된 입력입니다.\n 다시 입력 해 주세요.");
-						}
+			}
+			if(isP2Game) {
+				System.out.println("p2 player님 카드를 받으시겠습니까?(1.yes 2.no)");
+				if(1==Integer.parseInt(sc.nextLine())) {
+					getP2Card();
+					if(p2Scoure()>21) {
+						break;
 					}
 				}else {
-					//로그인 실패
-					System.out.println("아이디와 비밀 번호를 다시 확인 하세요.");
-					inputId=null;
+					isP2Game=false;
 				}
+			}
+			if(!isP1Game&&!isP2Game) {
+				break;
+			}
 			
+			displayP1Card();
+			displayP2Card();
+		}
+	}
+	
+	public static void win() {
+		if(p1Scoure()>21 || p1Scoure()<p2Scoure()) {
+			System.out.println("p2가 승리하였습니다.");
+		}else if(p2Scoure()>21 || p1Scoure()>p2Scoure()) {
+			System.out.println("p1이 승리하였습니다.");
+		}else {
+			System.out.println("동점으로 무승부 입니다.");
+		}
+	}
+	public static void main(String[] args) {
+		while(true) {
+			newCard();
+			mixCard();
+			play();
+			win();
+			
+			System.out.println("게임을 계속 하시겠습니까?(1.yes 2.no)");
+			int i=Integer.parseInt(sc.nextLine());
+			if(i==2) {
+				break;
 			}
 		}	
+		
+		
 	}
 
 }
